ac3fcfbb6446f24115db94424c0fbfae1c720bdd
wangjj <wangjj886688@163.com>
2015-12-03 23:48:56 +0800

    cameralibrate

--- opencv2_cameraCalibrate/opencv2_cameraCalibrate/cameraCalibrate.h
+++ opencv2_cameraCalibrate/opencv2_cameraCalibrate/cameraCalibrate.h
@@ -9,12 +9,88 @@ class camerCalibrate
 public:
 	camerCalibrate();
 	~camerCalibrate();
+	int addChessboardPoints(const vector <string>&filelist,Size &boardSize)
+	{
+		// the points on the chessboard
+		vector<Point2f> imageCorners;
+		vector<Point3f> objectCorners;
+		// 3D Scene Points:
+		// Initialize the chessboard corners in the chessboard reference frame
+		// 
+		// The corners are at 3D location (X,Y,Z)= (i,j,0)
+		for (int i = 0; i < boardSize.height; i++){
+			for (int j = 0; j < boardSize.width;j++)
+			{
+				objectCorners.push_back(Point3f(i, j, 0.0f));
+			}
+		}
+		//2D Scene Points
+		Mat image;// to contain chessboard image
+		int success = 0;
+		//for all viewpoints
+		for (int i = 0; i < filelist.size(); i++){
+			image = imread(filelist[i], 0); // Open the image
+			// Get the chessboard corners
+			bool found = findChessboardCorners(image, boardSize, imageCorners);
+			// Get subpixel accuracy on the corners
+			cornerSubPix(image, imageCorners, Size(5, 5), Size(-1, -1),TermCriteria(TermCriteria::MAX_ITER+TermCriteria::EPS,30,0.1));
+			if (imageCorners.size() == boardSize.area()){
+				addPoints(imageCorners, objectCorners);
+				success++;
+			}
+		}
+		return success;
+	}
+	void addPoints(const vector<Point2f> &imageCorners,const vector<Point3f> &objectCorners){
+		//2D points
+		imagePoints.push_back(imageCorners);
+		objectPoints.push_back(objectCorners);
+	}
+	// Calibrate the camera
+	// returns the re-projection error
+	double calibrate(Size &imageSize){
+		mustInitUndistort = true;// undistorter must be reinitialized
+		vector<Mat> rvecs, tvecs;
+		return calibrateCamera(objectPoints, imagePoints, imageSize, camerMatrix, distCoeffs, rvecs, tvecs, flag);
+
+	}
+	Mat remap_(Mat &image){
+		Mat undistorted;
+		if (mustInitUndistort){
+			initUndistortRectifyMap(camerMatrix, distCoeffs, Mat(), Mat(), image.size(), CV_32FC1, map1, map2);
+			mustInitUndistort = false;
+		}
+		remap(image, undistorted, map1, map2, INTER_LINEAR);
+		return undistorted;
+	}
+
+	// Getters
+	Mat getCameraMatrix() { return camerMatrix; }
+	Mat getDistCoeffs()   { return distCoeffs; }
+
+	void setCalibrationFlag(bool radial8CoeffEnabled, bool tangentialParamEnabled) {
+
+		// Set the flag used in cv::calibrateCamera()
+		flag = 0;
+		if (!tangentialParamEnabled) flag += CV_CALIB_ZERO_TANGENT_DIST;
+		if (radial8CoeffEnabled) flag += CV_CALIB_RATIONAL_MODEL;
+	}
 
 private:
+	vector<vector<Point3f>>objectPoints;//世界坐标
+	vector <vector<Point2f>> imagePoints;//像素坐标
+	//输出矩阵
+	Mat camerMatrix;
+	Mat distCoeffs;
+	//标定方式
+	int flag;
+	//用于图像去畸变
+	Mat map1, map2;
+	bool mustInitUndistort;
 
 };
 
-camerCalibrate::camerCalibrate()
+camerCalibrate::camerCalibrate() :flag(0), mustInitUndistort(true)
 {
 }
 
--- opencv2_cameraCalibrate/opencv2_cameraCalibrate/main.cpp
+++ opencv2_cameraCalibrate/opencv2_cameraCalibrate/main.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <iomanip>
+#include <opencv2/features2d/features2d.hpp>
+#include "cameraCalibrate.h"
 using namespace cv;
 using namespace std;
 int main()
 {
-	Mat image = imread("chessboard07.jpg");
+	Mat image = imread("../chessboards/chessboard07.jpg");
 	vector<Point2f> imageCorners;
 	Size boardSize(6, 4);
 	bool found = findChessboardCorners(image, boardSize, imageCorners);
 	drawChessboardCorners(image, boardSize, imageCorners, found);
 	imshow("image", image);
+
+	namedWindow("Image");
+	Mat Image;
+	vector<string> filelist;
+	for (int i = 1; i < 20; i++){
+		stringstream str1;
+		str1 << "../chessboards/chessboard" << setw(2) << setfill('0') << i << ".jpg";
+		cout << str1.str() << endl;
+		filelist.push_back(str1.str());
+		Image = imread(str1.str(), 0);
+		imshow("Image", Image);
+		waitKey(100);
+	}
+	camerCalibrate camerCalibrator;
+	Size BoardSize(6, 4);
+	camerCalibrator.addChessboardPoints(filelist, BoardSize);
+	camerCalibrator.calibrate(Image.size());
+	// Image Undistortion
+	Image = imread(filelist[6]);
+	Mat uImage = camerCalibrator.remap_(Image);
+	// display camera matrix
+	Mat camerMatrix = camerCalibrator.getCameraMatrix();
+	std::cout << " Camera intrinsic: " << camerMatrix.rows << "x" << camerMatrix.cols << std::endl;
+	std::cout << camerMatrix.at<double>(0, 0) << " " << camerMatrix.at<double>(0, 1) << " " << camerMatrix.at<double>(0, 2) << std::endl;
+	std::cout << camerMatrix.at<double>(1, 0) << " " << camerMatrix.at<double>(1, 1) << " " << camerMatrix.at<double>(1, 2) << std::endl;
+	std::cout << camerMatrix.at<double>(2, 0) << " " << camerMatrix.at<double>(2, 1) << " " << camerMatrix.at<double>(2, 2) << std::endl;
+
+	imshow("Original Image", Image);
+	imshow("Undistorted Image", uImage);
+
+
 	waitKey(0);
 	return 0;
 }
